234b97ec0ac9760368c8c2bf925d36a8b9b8c843
Scott Imandt <scottimandt@gmail.com>
2023-06-13 18:58:15 -0400

    Created obj oriented project overview for json overview editing; added edit function to existing files; added append function for exisiting files

---
+++ ProjectOverviewJSON.py
@@ -0,0 +1,70 @@
+import json
+import os
+
+class ProjectOverviewJSON:
+
+    jsonFile = []
+
+    def __init__(self):
+        self.loadJSONFile()
+
+
+    def loadJSONFile(self):
+
+        path = r"JsonFiles"
+        os.chdir(path)
+
+        for file in os.listdir():
+            if (file.endswith('.json')):
+
+                with open(os.path.abspath(file)) as f:
+                    file_contents = json.load(f)
+
+                    self.jsonFile.append(file_contents)
+
+    def saveJSONFile(self):
+
+        index = 0
+        for file in os.listdir():
+            if (file.endswith('.json')):
+
+                with open(os.path.abspath(file), 'w') as outfile:
+                    json.dump(self.jsonFile[index], outfile)
+                index += 1
+
+    def getIndexedJSON(self, index1, index2):
+        return self.jsonFile[index1][index2]
+    
+    def editJSONFile(self, JSON, switch, content):
+
+        match switch:
+            case "title":
+                JSON.update({'title': content})
+                pass
+            case "details":
+                JSON.update({'details': content})
+                pass
+            case "imagesrc":
+                JSON.update({'imagesrc': content})
+                pass
+            case "link":
+                JSON.update({'link': content})
+                pass
+#            case "index":
+#                JSON.update({'index': content})
+#                pass
+            case "id":
+                JSON.update({'id': content})
+                pass
+            case _:
+                print("The switch cannont be found")
+
+        pass
+
+    def addToJSONFile(self,JSONIndex, title, details, imagesrc, link):
+
+        id = len(self.jsonFile[JSONIndex]) + 1
+        
+        self.jsonFile[JSONIndex].append({'title':title, 'details':details, 'imagesrc':imagesrc, 'link':link, 'id': id})
+        
+        pass
--- main.py
+++ main.py
@@ -1,41 +1,10 @@
-import json
-import os
-
-
-def loadJSONFile():
-
-    path = r"JsonFiles"
-    os.chdir(path)
-
-    for file in os.listdir():
-        if (file.endswith('.json')):
-
-            with open(os.path.abspath(file)) as f:
-                file_contents = json.load(f)
-
-                jsonFile.append(file_contents)
-
-
-def saveJSONFile():
-
-    # access can be achieved by using jsonFile[index][index]
-    jsonFile[0][0].update({'title': "This re-write WORKED"})
-    print(jsonFile[0][0].get('title'))
-
-    # path = r"JsonFiles"
-    # os.chdir(path)
-    index = 0
-    for file in os.listdir():
-        if (file.endswith('.json')):
-
-            with open(os.path.abspath(file), 'w') as outfile:
-                json.dump(jsonFile[index], outfile)
-            index += 1
-
-#    'JsonFiles\ProjectOverviewDataCourse.json'
+import ProjectOverviewJSON
 
 
 if __name__ == "__main__":
-    jsonFile = []
-    loadJSONFile()
-    saveJSONFile()
+    
+    obj1 = ProjectOverviewJSON.ProjectOverviewJSON()
+    temp = obj1.getIndexedJSON(0,0)
+    obj1.editJSONFile(temp, 'title', 'THis should work in theory')
+    obj1.addToJSONFile(0,"This is the Title for the addtoJSON method", "","","")
+    obj1.saveJSONFile()
